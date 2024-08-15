7c917a32e8a2f399dd08937b347d60d136d5167d
akolle <andykoller55@yahoo.com>
2024-08-14 21:11:54 -0400

    pageId logic added

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,6 +3,7 @@ const AppContext = createContext()
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSetbarOpen] = useState(false)
+  const [pageId, setPageId] = useState(null)
 
   const openSidebar = () => {
     setIsSetbarOpen(true)
@@ -13,7 +14,9 @@ export const AppProvider = ({ children }) => {
   }
 
   return (
-    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar }}>
+    <AppContext.Provider
+      value={{ isSidebarOpen, openSidebar, closeSidebar, pageId, setPageId }}
+    >
       {children}
     </AppContext.Provider>
   )
